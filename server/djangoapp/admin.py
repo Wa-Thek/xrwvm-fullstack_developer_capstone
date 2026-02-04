0a3442b7c3ee3d2dbd79e3aefd804845c2763b43
Wa-Thek <wathek.ghenimi.d50@gmail.com>
2026-02-04 14:11:30 -0500

    Building the CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,19 @@
 # from django.contrib import admin
-# from .models import related models
+# from .models import CarMake, CarModel
 
+# # Define an inline class for CarModel to be displayed within CarMake admin page
+# class CarModelInline(admin.TabularInline):
+#     model = CarModel
+#     extra = 1  # Number of empty CarModel forms to display
 
-# Register your models here.
+# # Customize the CarMake admin interface
+# class CarMakeAdmin(admin.ModelAdmin):
+#     inlines = [CarModelInline]  # Show CarModels as inlines
+#     list_display = ('name', 'description')  # Columns to display in list view
+
+# # Register models with the admin site
+# admin.site.register(CarMake, CarMakeAdmin)
+# admin.site.register(CarModel)
 
 # CarModelInline class
 
@@ -11,3 +22,10 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
